650c63a3b8e64378cd12532e2864f63fcfb86273
pappferencgabor <p.feco2005@gmail.com>
2023-05-22 12:43:16 +0200

    Ora vegi allapot

--- ConVizibicikli/Kolcsonzes.cs
+++ ConVizibicikli/Kolcsonzes.cs
@@ -14,6 +14,22 @@ namespace ConVizibicikli
         int ePerc;
         int vOra;
         int vPerc;
+        
+        public int IdoHossz()
+        {
+            return VOra * 60 + VPerc - (eOra * 60 + ePerc);
+        } 
+
+        public Kolcsonzes(string csvSor)
+        {
+            var mezok = csvSor.Split(';');
+            this.nev = mezok[0];
+            this.jazon = mezok[1][0];
+            this.eOra = int.Parse(mezok[2]);
+            this.ePerc = int.Parse(mezok[3]);
+            this.vOra = int.Parse(mezok[4]);
+            this.vPerc = int.Parse(mezok[2]);
+        }
 
         public Kolcsonzes(string nev, char jazon, int eora, int eperc, int vora, int vperc)
         {
--- ConVizibicikli/Program.cs
+++ ConVizibicikli/Program.cs
@@ -1,12 +1,76 @@
-﻿namespace ConVizibicikli
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConVizibicikli
 {
     internal class Program
     {
-        List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+        static List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
 
         static void Main(string[] args)
         {
+            /*
+            // Hagyományos
+            StreamReader sr = new StreamReader("Datasources\\kolcsonzesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(';');
+                Kolcsonzes ujAdat = new Kolcsonzes(mezok[0],
+                                                   mezok[1][0],
+                                                   int.Parse(mezok[2]),
+                                                   int.Parse(mezok[3]),
+                                                   int.Parse(mezok[4]),
+                                                   int.Parse(mezok[5]));
+            }
+            sr.Close();
+            */
             
+
+            // LINQ + Foreach + Konstruktor
+            /*foreach (var sor in File.ReadAllLines("Datasources\\kolcsonzesek.txt"))
+            {
+                kolcsonzesek.Add(new Kolcsonzes(sor));
+            }
+            */
+
+            // LINQ
+            kolcsonzesek = File.ReadAllLines("Datasources\\kolcsonzesek.txt").Skip(1).Select(x => new Kolcsonzes(x)).ToList();
+
+            // 5.
+            Console.WriteLine($"5. feladat: Napi kölcsönzések száma: {kolcsonzesek.Count()}");
+
+            // 6.
+            Console.Write($"6. feladat: Kérek egy nevet: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine($"\t{nev} kölcsönzései:");
+
+            if (kolcsonzesek.Count(x => x.Nev == nev) == 0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző");
+            }
+            else
+            {
+                /*foreach (var akt in kolcsonzesek.Where(x => x.Nev == nev))
+                {
+                    Console.WriteLine($"\t{akt.EOra}:{akt.EPerc}-{akt.VOra}:{akt.VPerc}");
+                }*/
+
+                kolcsonzesek.Where(x => x.Nev == nev).ToList().ForEach(x => Console.WriteLine($"\t{x.EOra}:{x.EPerc}-{x.VOra}:{x.VPerc}"));
+            }
+
+            // 7.
+
+            // 8.
+            Console.WriteLine($"8. feladat: A napi bevétel: {2400 * kolcsonzesek.Sum(x => x.IdoHossz())/ 30 + 1} Ft");
+
+            // 9.
+
+
+            // 10.
+            kolcsonzesek.GroupBy(x => x.JAzon).OrderBy(x => x.Key).ToList().ForEach(x => Console.WriteLine($"\t{x.Key} - {x.Count()}"));
         }
     }
 }
